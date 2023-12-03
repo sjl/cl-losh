b35b182e6747222735b8eda524cc5caed79d7a1b
Steve Losh <steve@stevelosh.com>
2023-12-03 17:29:39 -0500

    Add #! and #; readers

--- src/readtable.lisp
+++ src/readtable.lisp
@@ -1,4 +1,18 @@
 (in-package :losh.readtable)
 
+
+(defun sharp-semicolon-reader (stream sub-char numarg)
+  (declare (ignore sub-char numarg))
+  (loop :while (read-line stream nil nil))
+  (values))
+
+(defun shebang-reader (stream sub-char numarg)
+  (declare (ignore sub-char numarg))
+  (read-line stream)
+  (values))
+
 (named-readtables:defreadtable losh
-  (:merge :standard losh.hash-tables::hash-table-constructor-syntax))
+  (:merge :standard losh.hash-tables::hash-table-constructor-syntax)
+  (:macro-char #\# :dispatch)
+  (:dispatch-macro-char #\# #\; #'sharp-semicolon-reader)
+  (:dispatch-macro-char #\# #\! #'shebang-reader))
